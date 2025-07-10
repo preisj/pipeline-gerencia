f49a9ecc2e1bf392f46e1a605a0e60f298b2eba7
Pedro Reis Jung <pedro.reis.jung@gmail.com>
2025-07-10 12:12:18 -0300

    teste

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,8 +1,15 @@
-const app = require('./app');
+const express = require('express');
+const app = express();
+app.use(express.json());
 
-const PORT = 3001;
+const ENV_PREFIX = process.env.ENV_PREFIX || '';
+
+app.get(`${ENV_PREFIX}/health`, (req, res) => res.send('OK'));
+
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
-  console.log(`Backend rodando na porta ${PORT}`);
+  console.log(`Backend rodando na porta ${PORT} com prefixo ${ENV_PREFIX}`);
 });
+
 
 
